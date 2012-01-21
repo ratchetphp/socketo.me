97dead1b468fc347a2bdcc01a630e56d1c7b3b4c
Chris Boden <cboden@gmail.com>
2012-01-21 00:46:01 -0500

    Init
    Twitter Bootstrap
    Home page content
    HTML5 WebSocket as favicon

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Ratchet - WebSockets for PHP | Home</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le styles -->
+    <link href="/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+      }
+    </style>
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  </head>
+
+  <body>
+
+    <div class="topbar">
+      <div class="topbar-inner">
+        <div class="container-fluid">
+          <a class="brand" href="/">Ratchet</a>
+          <ul class="nav">
+            <li class="active"><a href="/">Home</a></li>
+            <li><a href="/docs">Documentation</a></li>
+            <li><a href="/api/namespace-Ratchet.html">API Docs</a></li>
+            <li><a href="/demos">Demos</a></li>
+            <li><a href="https://groups.google.com/forum/#!forum/ratchet-php">Google Groups</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+    require __DIR__ . '/header.php';
+?>
+
+    <div class="container">
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+        <h1>Ratchet</h1>
+        <h2>WebSockets for PHP</h2>
+        <p>Ratchet is a component library for PHP that provides developers with the tools to create real time, bi-directional applications between clients and servers. This is not your Grandfather's Internet. </p>
+        <p><a class="btn primary large" href="/docs">Learn more &raquo;</a></p>
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="span-one-third">
+          <h2>Fast & Easy</h2>
+          <p>Write your own chat (aka "Hello World!" for sockets) application in a matter of minutes. </p>
+          <p>After understanding "the new flow" - event driven programming, compared to traditional HTTP request/response - writing any application on top of Ratchet becomes fast and easy.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+
+        <div class="span-one-third">
+          <h2>Decoupled</h2>
+           <p>Ratchet provides many components to pick and choose from for you to use. 
+           <p>For example, Ratchet comes with a Server component; a CLI based I/O server written in PHP...you really don't want to use that for a live site - but it makes development/testing easy. </p>
+           <p>When it comes time to go live, take out the five lines of code that implement Server, and hitch your application on to something more robust, like Nginx.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+       </div>
+
+        <div class="span-one-third">
+          <h2>Integrated</h2>
+          <p>Ratchet is fully PSR-0 compliant, so it naturally plays well with others. Ratchet leverages components of Symfony2, making development familiar to many.</p>
+          <p>Unit tests ensure each part of Ratchet behaves as intended.</p>
+          <p>Ratchet provides a Session component to allow persistence between your website and your WebSocket application.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+      </div>
+
+      <footer>
+        <p><a href="http://www.w3.org/html/logo/"><img src="http://www.w3.org/html/logo/badge/html5-badge-h-connectivity.png" width="133" height="64" alt="HTML5 Powered with Connectivity / Realtime" title="HTML5 Powered with Connectivity / Realtime"></a></p>
+      </footer>
+
+    </div> <!-- /container -->
+
+<?php
+    require __DIR__ . '/footer.php';
