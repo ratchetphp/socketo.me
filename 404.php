d613b4228ee8021140355ea60871067294aa8f89
Chris Boden <cboden@gmail.com>
2012-01-21 01:05:46 -0500

    404
    Someone feed the Tumblebeast!

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+    require __DIR__ . '/header.php';
+?>
+
+    <div class="container">
+      <div class="hero-unit" style="text-align: center">
+        <img src="/404.png" width="319" height="402">
+      </div>
+
+      <footer>
+        <p>404 image courtesy of <a href="http://TheOatmeal.com">Matthew Inman</a></p>
+      </footer>
+    </div>
+
+<?php
+    require __DIR__ . '/footer.php';
