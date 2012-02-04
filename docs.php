208688de8113fa96571ec1cea97090739c1f4ac4
Chris Boden <cboden@gmail.com>
2012-02-04 14:57:32 -0500

    Docs
    Added a placeholder for documentation

---
+++ docs.php
@@ -0,0 +1,40 @@
+<?php
+    require __DIR__ . '/header.php';
+?>
+
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <div class="span3">
+          <div class="well sidebar-nav" style="margin-top: 65px;">
+            <ul class="nav nav-list">
+              <li class="nav-header">Getting Started</li>
+              <li class="active"><a href="#">Installation</a></li>
+              <li><a href="#">Design Philosophy</a></li>
+
+              <li class="nav-header">Components</li>
+              <li><a href="#">WebSocketComponent</a></li>
+              <li><a href="#">SessionComponent</a></li>
+              <li><a href="#">WAMPServerComponent</a></li>
+              <li><a href="#">IOServerComponent</a></li>
+              <li><a href="#">IPBlackListComponent</a></li>
+            </ul>
+          </div><!--/.well -->
+
+          <div class="well sidebar-nav">
+            <ul class="nav nav-list">
+                <li class="nav-header">Resources</li>
+                <li><a href="https://groups.google.com/forum/#!forum/ratchet-php">Google Groups</a></li>
+                <li><a href="https://github.com/cboden/Ratchet/issues">Submit an Issue</a></li>
+                <li><a href="https://github.com/cboden/Ratchet">Contribute</a></li>
+            </ul>
+          </div>
+        </div><!--/span-->
+
+
+        <div class="span9">
+        </div><!--/span-->
+      </div><!--/row-->
+
+
+<?php
+    require __DIR__ . '/footer.php';
