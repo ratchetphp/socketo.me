3515cadd73d1dd712a87ef99d100c4a1f0644591
Jon Rundle <jon@resolutionim.com>
2012-05-17 18:39:24 -0400

    [STYLE] - Slicing of chat UI

--- chat/index.php
+++ chat/index.php
@@ -1,3 +1,55 @@
 <section id="chat">
 
+	<div id="colA">
+		<a href="#" class="groupHead open">Channel 1</a>
+		<ul class="users">
+			<li><span>Indicator</span>Jon Rundle</li>
+			<li><span>Indicator</span>Chris Boden</li>
+			<li><span>Indicator</span>Mike Almond</li>
+		</ul>
+		<a href="#" class="groupHead">Channel 2 <span class="notifications">2</span></a>
+		<a href="#" class="groupHead">Channel 3</a>
+		<a href="#" class="groupHead">Channel 4</a>
+	</div>
+	
+	<div id="colB">
+		<div class="chatWindow">
+			<div class="comment">
+				<h2>Jon Rundle<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment mine">
+				<h2>Jon Rundle<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment">
+				<h2>Jon Rundle<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment">
+				<h2>Jon Rundle<br /><span>3 min ago</span></h2>
+				<p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere consectetur est at lobortis.</p>
+			</div>
+			<div class="comment mine">
+				<h2>Jon Rundle<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment">
+				<h2>Jon Rundle<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment mine">
+				<h2>Jon Rundle<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment">
+				<h2>Jon Rundle<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+		</div>
+		<form>
+			<input type="text" placeholder="Type here…" />
+		</form>
+	</div>
+
 </section>
