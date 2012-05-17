4e5edc97f72a553c266e7ef2270d108e0a5f4efe
Jon Rundle <jon@resolutionim.com>
2012-05-17 19:13:09 -0400

    [JS] - Javascript for flipping between chats

--- chat/chat.js
+++ chat/chat.js
@@ -1,9 +1,23 @@
 $(function() {
 	$('.groupHead:first').next('.users').slideToggle();
 	$('.groupHead:first').addClass('open');
+	$('.chatWindow:first').fadeIn();
+	$('.chatWindow:first').addClass('active');
 	
 	$('.groupHead').click(function() {
+		$('.groupHead').each(function() {
+			$('.open').next('.users').slideUp();
+			$('.open').removeClass('open');
+		});
 		$(this).next('.users').slideToggle();
 		$(this).toggleClass('open');
+		
+		$('.active').each(function() {
+			$(this).fadeOut();
+			$(this).removeClass('active');
+		});
+		var channel = $(this).attr('data-channel');
+		$('#' + channel).fadeIn();
+		$('#' + channel).addClass('active');
 	}); 
 });
--- chat/index.php
+++ chat/index.php
@@ -1,25 +1,25 @@
 <section id="chat">
 
 	<div id="colA">
-		<a href="#" class="groupHead">Channel 1</a>
+		<a href="#" class="groupHead" data-channel="channel1">Channel 1</a>
 		<ul class="users">
 			<li><span>Indicator</span>Jon Rundle</li>
 			<li><span>Indicator</span>Chris Boden</li>
 			<li><span>Indicator</span>Mike Almond</li>
 		</ul>
-		<a href="#" class="groupHead">Channel 2 <span class="notifications">2</span></a>
+		<a href="#" class="groupHead" data-channel="channel2">Channel 2 <span class="notifications">2</span></a>
 		<ul class="users">
 			<li><span>Indicator</span>Jon Rundle</li>
 			<li><span>Indicator</span>Chris Boden</li>
 			<li><span>Indicator</span>Mike Almond</li>
 		</ul>
-		<a href="#" class="groupHead">Channel 3</a>
+		<a href="#" class="groupHead" data-channel="channel3">Channel 3</a>
 		<ul class="users">
 			<li><span>Indicator</span>Jon Rundle</li>
 			<li><span>Indicator</span>Chris Boden</li>
 			<li><span>Indicator</span>Mike Almond</li>
 		</ul>
-		<a href="#" class="groupHead">Channel 4</a>
+		<a href="#" class="groupHead" data-channel="channel4">Channel 4</a>
 		<ul class="users">
 			<li><span>Indicator</span>Jon Rundle</li>
 			<li><span>Indicator</span>Chris Boden</li>
@@ -30,7 +30,7 @@
 	</div>
 	
 	<div id="colB">
-		<div class="chatWindow">
+		<div id="channel1" class="chatWindow">
 			<div class="comment">
 				<h2>Jon Rundle<br /><span>3 min ago</span></h2>
 				<p>Donec id elit non mi porta gravida at eget metus.</p>
@@ -62,6 +62,84 @@
 			<div class="comment">
 				<h2>Jon Rundle<br /><span>3 min ago</span></h2>
 				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+		</div>
+		<div id="channel2" class="chatWindow">
+			<div class="comment">
+				<h2>Chris Boden<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment">
+				<h2>Chris Boden<br /><span>3 min ago</span></h2>
+				<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+			</div>
+			<div class="comment mine">
+				<h2>Chris Boden<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment">
+				<h2>Chris Boden<br /><span>3 min ago</span></h2>
+				<p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere consectetur est at lobortis.</p>
+			</div>
+			<div class="comment">
+				<h2>Chris Boden<br /><span>3 min ago</span></h2>
+				<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+			</div>
+			<div class="comment">
+				<h2>Chris Boden<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment mine">
+				<h2>Chris Boden<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment">
+				<h2>Chris Boden<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+		</div>
+		<div id="channel3" class="chatWindow">
+			<div class="comment mine">
+				<h2>Mike Almond<br /><span>3 min ago</span></h2>
+				<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+			</div>
+			<div class="comment mine">
+				<h2>Mike Almond<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment">
+				<h2>Mike Almond<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment">
+				<h2>Mike Almond<br /><span>3 min ago</span></h2>
+				<p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere consectetur est at lobortis.</p>
+			</div>
+			<div class="comment mine">
+				<h2>Mike Almond<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment">
+				<h2>Mike Almond<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment mine">
+				<h2>Mike Almond<br /><span>3 min ago</span></h2>
+				<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+			</div>
+			<div class="comment">
+				<h2>Mike Almond<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+		</div>
+		<div id="channel4" class="chatWindow">
+			<div class="comment">
+				<h2>Jonathan Kochis<br /><span>3 min ago</span></h2>
+				<p>Donec id elit non mi porta gravida at eget metus.</p>
+			</div>
+			<div class="comment mine">
+				<h2>Jonathan Kochis<br /><span>3 min ago</span></h2>
+				<p>Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Curabitur blandit tempus porttitor.</p>
 			</div>
 		</div>
 		<form>
